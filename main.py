6f32e1fe1ef85958f6d6560e8ea830bc088e5376
CJ Sturgess <cj@sturgess.co>
2023-04-03 10:24:58 -0700

    init

---
+++ main.py
@@ -0,0 +1,10 @@
+# A charmingly simple application to view and edit Minecraft-specific NBT files.
+import tkinter
+
+def main():
+    window = tkinter.Tk()
+    window.title("NBTree")
+
+
+if __name__ == "__main__":
+    main()
